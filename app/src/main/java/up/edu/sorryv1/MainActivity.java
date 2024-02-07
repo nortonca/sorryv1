2f2d6c4ce9c47fe08e459f4dd8d332ea0941cdef
nortonca <nortoncarr3@gmail.com>
2024-02-06 19:30:26 -0800

    Initial commit

---
+++ app/src/main/java/up/edu/sorryv1/MainActivity.java
@@ -0,0 +1,17 @@
+package up.edu.sorryv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //ImageView imageView = findViewById(R.id.imageView);
+        //imageView.setImageResource(R.drawable.sorryb); // Replace 'your_image' with the actual image name
+    }
+}
